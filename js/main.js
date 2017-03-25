7b292fba7558e3ca67480cc8ea0422377c138545
“Richard Middleton” <Richard@richardmiddleton.me>
2017-03-25 15:42:29 +0100

    tidied JS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 var units = "&units=metric";
 var endUnit = "C";
 
-jQuery(function($) {
-// farenheit or celsius
+jQuery(function ($) {
+  // farenheit or celsius
   $('.celsius').on('click', function () {
     units = "&units=metric";
     endUnit = "C";
-    $(this).css({'color': 'white'});
+    $(this).css({
+      'color': 'white'
+    });
     $('.farenheit').css('color', 'rgba(72, 72, 72, 1.0)');
     weather();
   });
@@ -14,14 +16,18 @@ jQuery(function($) {
   $('.farenheit').on('click', function () {
     units = "&units=imperial";
     endUnit = "F";
-    $(this).css({'color': 'white'});
+    $(this).css({
+      'color': 'white'
+    });
     $('.celsius').css('color', 'rgba(72, 72, 72, 1.0)');
     weather();
   });
 
   // New quote on start
   $(document).ready(function () {
-    $('.celsius').css({'color': 'white'});
+    $('.celsius').css({
+      'color': 'white'
+    });
     $('.farenheit').css('color', 'rgba(72, 72, 72, 1.0)');
     weather();
   });
@@ -29,58 +35,65 @@ jQuery(function($) {
 
   function weather() {
 
-// Get Location
+    // Get Location
     $.getJSON('http://ip-api.com/json', function (response) {
-        var city = response.city;
-        displayWeather(city);
+      var city = response.city;
+      var country = response.country;
+      displayWeather(city, country);
     });
 
 
-    function displayWeather(city) {
-        var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
-        var API_Key = "&APPID=1f3e30098d59daa0ee84d36dca533728";
-        var full_API_Link = weatherAPI + city + units + API_Key;
-        
-        $.getJSON(full_API_Link, function (response) {
+    function displayWeather(city, country) {
+      var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
+      var API_Key = "&APPID=1f3e30098d59daa0ee84d36dca533728";
+      var full_API_Link = weatherAPI + city + units + API_Key;
+
+      $.getJSON(full_API_Link, function (response) {
 
         // Interpret data 
-        var location = response.name;
-        var country = response.sys.country;
         var temp_c = Math.round(response.main.temp);
         var description = response.weather[0].description;
         var icon = response.weather[0].icon;
 
-          // Switch to ION icons' icon pack
-          if (icon == '01d') {
-            icon = "<i class='ion-ios-sunny-outline large'></i>";
-          } else if (icon == '01n') {
-            icon = "<i class='ion-ios-moon-outline large'></i>";
-          } else if (icon == '02d') {
-            icon = "<i class='ion-ios-partlysunny-outline large'></i>";
-          } else if (icon == '02n') {
-            icon = "<i class='ion-ios-cloudy-night-outline large'></i>";
-          } else if (icon == '03d' || icon == '03n') {
-            icon = "<i class='ion-ios-cloud-outline large'></i>";
-          } else if (icon == '04d' || icon == '04n') {
-            icon = "<i class='ion-ios-cloud large'></i>";
-          } else if (icon == '09d' || icon == '09n') {
-            icon = "<i class='ion-ios-rainy-outline large'></i>";
-          } else if (icon == '10d' || icon == '10n') {
-            icon = "<i class='ion-ios-rainy-outline large'></i>";
-          } else if (icon == '11d' || icon == '11n') {
-            icon = "<i class='ion-ios-thunderstorm-outline large'></i>";
-          } else if (icon == '13d' || icon == '13n') {
-            icon = "<i class='ion-ios-snowy large'></i>";
-          } else if (icon == '50d' || icon == '50n') {
-            icon = "<i class='ion-ios-cloud-outline large'></i>";
-          }
+        replace(icon, city, country, temp_c, endUnit, description);
 
-             // Add to Dom
-          $('.weather-place').html(icon + "<p>" + location + ", " + country + "</p><p>" + temp_c + "°" + endUnit + "</p>" + "<p>" + description).fadeIn(400) + "</p>";
+      });
+    }
+  };
 
-          });
-        }
-  }
+  function replace(icon, city, country, temp_c, endUnit, description) {
+    
+    // Switch to ION icons' icon pack
+    if (icon == '01d') {
+      icon = "<i class='ion-ios-sunny-outline large'></i>";
+    } else if (icon == '01n') {
+      icon = "<i class='ion-ios-moon-outline large'></i>";
+    } else if (icon == '02d') {
+      icon = "<i class='ion-ios-partlysunny-outline large'></i>";
+    } else if (icon == '02n') {
+      icon = "<i class='ion-ios-cloudy-night-outline large'></i>";
+    } else if (icon == '03d' || icon == '03n') {
+      icon = "<i class='ion-ios-cloud-outline large'></i>";
+    } else if (icon == '04d' || icon == '04n') {
+      icon = "<i class='ion-ios-cloud large'></i>";
+    } else if (icon == '09d' || icon == '09n') {
+      icon = "<i class='ion-ios-rainy-outline large'></i>";
+    } else if (icon == '10d' || icon == '10n') {
+      icon = "<i class='ion-ios-rainy-outline large'></i>";
+    } else if (icon == '11d' || icon == '11n') {
+      icon = "<i class='ion-ios-thunderstorm-outline large'></i>";
+    } else if (icon == '13d' || icon == '13n') {
+      icon = "<i class='ion-ios-snowy large'></i>";
+    } else if (icon == '50d' || icon == '50n') {
+      icon = "<i class='ion-ios-cloud-outline large'></i>";
+    };
+
+
+    // Add to Dom
+    $('.weather-place').html(icon + "<p>" + city + ", " + country + "</p><p>" + temp_c + "°" + endUnit + "</p>" + "<p>" + description).fadeIn(400) + "</p>";
+
+  };
+
 
   // Get time and change background
   var currentTime = new Date().getHours();
@@ -94,4 +107,4 @@ jQuery(function($) {
       document.body.background = "images/night.jpg";
     }
   }
-  });
+});
