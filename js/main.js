a1c5d5212b5bbdddff35720297e8c3dd55bc582c
“Richard Middleton” <Richard@richardmiddleton.me>
2017-03-25 15:10:38 +0100

    functioning on http only

--- js/main.js
+++ js/main.js
@@ -1,115 +1,97 @@
-var geoAddress;
 var units = "&units=metric";
-var endUnit= "C";
+var endUnit = "C";
 
+jQuery(function($) {
 // farenheit or celsius
-
-$('.celsius').on('click', function () {
+  $('.celsius').on('click', function () {
     units = "&units=metric";
     endUnit = "C";
     $(this).css({'color': 'white'});
     $('.farenheit').css('color', 'rgba(72, 72, 72, 1.0)');
     weather();
-});
+  });
 
-
-$('.farenheit').on('click', function () {
+  $('.farenheit').on('click', function () {
     units = "&units=imperial";
     endUnit = "F";
     $(this).css({'color': 'white'});
     $('.celsius').css('color', 'rgba(72, 72, 72, 1.0)');
     weather();
-});
+  });
 
-// New quote on start
-$(document).ready(function () {
+  // New quote on start
+  $(document).ready(function () {
     $('.celsius').css({'color': 'white'});
     $('.farenheit').css('color', 'rgba(72, 72, 72, 1.0)');
     weather();
-});
+  });
 
 
-function weather() {
+  function weather() {
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-
-      geoAddress = "http://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + units +"&appid=1f3e30098d59daa0ee84d36dca533728";
-
-      $.ajax({
-        url: geoAddress,
-        success: function (parsed_json) {
-
-          var location = parsed_json.name;
-          var country = parsed_json.sys.country;
-          var temp_c = Math.round(parsed_json.main.temp);
-          var description = parsed_json.weather[0].description;
-          var icon = parsed_json.weather[0].icon;
+// Get Location
+    $.getJSON('http://ip-api.com/json', function (response) {
+        var city = response.city;
+        displayWeather(city);
+    });
 
 
+    function displayWeather(city) {
+        var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?q=";
+        var API_Key = "&APPID=1f3e30098d59daa0ee84d36dca533728";
+        var full_API_Link = weatherAPI + city + units + API_Key;
+        
+        $.getJSON(full_API_Link, function (response) {
+
+        // Interpret data 
+        var location = response.name;
+        var country = response.sys.country;
+        var temp_c = Math.round(response.main.temp);
+        var description = response.weather[0].description;
+        var icon = response.weather[0].icon;
+
+          // Switch to ION icons' icon pack
           if (icon == '01d') {
             icon = "<i class='ion-ios-sunny-outline large'></i>";
-          }
-          if (icon == '01n') {
+          } else if (icon == '01n') {
             icon = "<i class='ion-ios-moon-outline large'></i>";
-          }
-
-          if (icon == '02d') {
+          } else if (icon == '02d') {
             icon = "<i class='ion-ios-partlysunny-outline large'></i>";
-          }
-
-          if (icon == '02n') {
+          } else if (icon == '02n') {
             icon = "<i class='ion-ios-cloudy-night-outline large'></i>";
-          }
-
-          if (icon == '03d' || icon == '03n') {
+          } else if (icon == '03d' || icon == '03n') {
             icon = "<i class='ion-ios-cloud-outline large'></i>";
-          }
-
-          if (icon == '04d' || icon == '04n') {
+          } else if (icon == '04d' || icon == '04n') {
             icon = "<i class='ion-ios-cloud large'></i>";
-          }
-
-          if (icon == '09d' || icon == '09n') {
+          } else if (icon == '09d' || icon == '09n') {
             icon = "<i class='ion-ios-rainy-outline large'></i>";
-
-          }
-          if (icon == '10d' || icon == '10n') {
+          } else if (icon == '10d' || icon == '10n') {
             icon = "<i class='ion-ios-rainy-outline large'></i>";
-          }
-
-          if (icon == '11d' || icon == '11n') {
+          } else if (icon == '11d' || icon == '11n') {
             icon = "<i class='ion-ios-thunderstorm-outline large'></i>";
-          }
-
-          if (icon == '13d' || icon == '13n') {
+          } else if (icon == '13d' || icon == '13n') {
             icon = "<i class='ion-ios-snowy large'></i>";
-          }
-
-          if (icon == '50d' || icon == '50n') {
+          } else if (icon == '50d' || icon == '50n') {
             icon = "<i class='ion-ios-cloud-outline large'></i>";
           }
 
-          // Add to Dom
+             // Add to Dom
           $('.weather-place').html(icon + "<p>" + location + ", " + country + "</p><p>" + temp_c + "°" + endUnit + "</p>" + "<p>" + description).fadeIn(400) + "</p>";
 
+          });
         }
-      })
-    })
-  }
   }
 
-// Get time and change background
-var currentTime = new Date().getHours();
+  // Get time and change background
+  var currentTime = new Date().getHours();
 
-if (7 <= currentTime && currentTime < 20) {
-  if (document.body) {
-    document.body.background = "https://static.pexels.com/photos/38989/pexels-photo-38989.jpeg";
+  if (7 <= currentTime && currentTime < 20) {
+    if (document.body) {
+      document.body.background = "images/day.jpg";
+    }
+  } else {
+    if (document.body) {
+      document.body.background = "images/night.jpg";
+    }
   }
-}
-else {
-  if (document.body) {
-    document.body.background = "https://static.pexels.com/photos/94363/pexels-photo-94363.jpeg";
-  }
-}
-
+  });
